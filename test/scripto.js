dd9c1a457f9a03ede3f1843ca769c5839266e0bb
Brian Meek <brian@here.video>
2022-01-14 07:38:03 -0800

    Convert to using ioredis; Set minimum NodeJS version to 12; Convert to using const; Add second lua file to tests

--- test/scripto.js
+++ test/scripto.js
@@ -1,145 +1,151 @@
-var path        = require('path');
-var assert      = require('assert');
-var redis       = require('redis');
-var Scripto     = require('../');
+const path = require("path");
+const assert = require("assert");
+const Redis = require("ioredis");
+const Scripto = require("../");
 
-var redisClient = redis.createClient();
-var scriptDir   = path.resolve(path.dirname(__filename), 'scripts');
+const scriptDir = path.resolve(path.dirname(__filename), "scripts");
 
-suite('Scripto', function() {
+const redisClient = new Redis({ lazyConnect: true });
 
-    suite('eval', function() {
+describe("Scripto", function () {
+  before(function (done) {
+    redisClient.connect(function (err) {
+      done();
+    });
+  });
+  after(function (done) {
+    redisClient.disconnect();
+    done();
+  });
 
-        test('running normally', _clean(function(done) {
-            
-            var s = new Scripto(redisClient);
-            s.loadFromDir(scriptDir);
-            s.eval('read-write', ['helloKey'], [200], function(err, result) {
+  afterEach(function (done) {
+    redisClient.flushdb(function (err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
 
-                assert.equal(err, null);
-                assert.equal(result, 200);
-                done();
-            });
-        }));
-
-        test('running non-existing script', _clean(function(done) {
-            
-            var s = new Scripto(redisClient);
-            s.loadFromDir(scriptDir);
-            s.eval('no-such-script', ['helloKey'], [200], function(err, result) {
-
-                assert.equal(err.message, 'NO_SUCH_SCRIPT');
-                assert.equal(result, undefined);
-                done();
-            });
-        }));
+  describe("Env working", function () {
+    it("ping", function (done) {
+      redisClient.ping(function (err, result) {
+        console.log(`redis ping ${JSON.stringify(result)}`);
+        assert.equal(err, null);
+        assert.equal(result, "PONG");
+        done();
+      });
+    });
+  });
+  describe("eval", function () {
+    it("running normally", function (done) {
+      const s = new Scripto(redisClient);
+      s.loadFromDir(scriptDir);
+      s.eval("read-write", ["helloKey"], [200], function (err, result) {
+        assert.equal(err, null);
+        assert.equal(result, 200);
+        done();
+      });
     });
 
-    suite('evalSha', function() {
+    it("running non-existing script", function (done) {
+      const s = new Scripto(redisClient);
+      s.loadFromDir(scriptDir);
+      s.eval("no-such-script", ["helloKey"], [200], function (err, result) {
+        assert.equal(err.message, "NO_SUCH_SCRIPT");
+        assert.equal(result, undefined);
+        done();
+      });
+    });
+  });
 
-        test('failed at initial call', _clean(function(done) {
-            
-            var s = new Scripto(redisClient);
-            s.loadFromDir(scriptDir);
-            s.evalSha('read-write', ['helloKey'], [200], function(err, result) {
-
-                assert.equal(err.message, 'NO_SUCH_SCRIPT_SHA');
-                assert.equal(result, undefined);
-                done();
-            });
-        }));
-
-        test('success at runs after script loaded (some millis later)', _clean(function(done) {
-            
-            var s = new Scripto(redisClient);
-            s.loadFromDir(scriptDir);
-
-            setTimeout(function() {
-                
-                s.evalSha('read-write', ['hello2Key'], [300], afterEvalSha);
-            }, 100);
-
-            function afterEvalSha(err, result) {
-
-                assert.equal(err, undefined);
-                assert.equal(result, 300);
-                done();
-            }
-        }));
+  describe("evalSha", function () {
+    it("failed at initial call", function (done) {
+      const s = new Scripto(redisClient);
+      s.loadFromDir(scriptDir);
+      s.evalSha("read-write", ["helloKey"], [200], function (err, result) {
+        assert.equal(err.message, "NO_SUCH_SCRIPT_SHA");
+        assert.equal(result, undefined);
+        done();
+      });
     });
 
-    suite('run', function() {
+    it("success at runs after script loaded (some millis later)", function (done) {
+      const s = new Scripto(redisClient);
+      s.loadFromDir(scriptDir);
 
-        test('success at initial call', _clean(function(done) {
-            
-            var s = new Scripto(redisClient);
-            s.loadFromDir(scriptDir);
-            s.run('read-write', ['helloKey'], [200], function(err, result) {
+      setTimeout(function () {
+        s.evalSha("read-write", ["hello2Key"], [300], afterEvalSha);
+      }, 100);
 
-                assert.equal(err, undefined);
-                assert.equal(result, 200);
-                done();
-            });
-        }));
+      function afterEvalSha(err, result) {
+        assert.equal(err, undefined);
+        assert.equal(result, 300);
+        done();
+      }
+    });
+  });
 
-        test('success at runs after script loaded (some millis later, then uses sha)', _clean(function(done) {
-            
-            var s = new Scripto(redisClient);
-            s.loadFromDir(scriptDir);
-
-            setTimeout(function() {
-                
-                s.run('read-write', ['hello2Key'], [300], afterEvalSha);
-            }, 100);
-
-            function afterEvalSha(err, result) {
-
-                assert.equal(err, undefined);
-                assert.equal(result, 300);
-                done();
-            }
-        }));
+  describe("run", function () {
+    it("success at initial call", function (done) {
+      const s = new Scripto(redisClient);
+      s.loadFromDir(scriptDir);
+      s.run("read-write", ["helloKey"], [200], function (err, result) {
+        assert.equal(err, undefined);
+        assert.equal(result, 200);
+        done();
+      });
     });
 
-    test('load scripts from an object', _clean(function(done) {
+    it("success at runs after script loaded (some millis later, then uses sha)", function (done) {
+      const s = new Scripto(redisClient);
+      s.loadFromDir(scriptDir);
 
-        var scripts = { 'script-one': 'return 1000;' };
-        var s = new Scripto(redisClient);
-        s.load(scripts);
+      setTimeout(function () {
+        s.run("read-write", ["hello2Key"], [300], afterEvalSha);
+      }, 100);
 
-        s.run('script-one', [], [], function(err, result) {
+      function afterEvalSha(err, result) {
+        assert.equal(err, undefined);
+        assert.equal(result, 300);
+        done();
+      }
+    });
+  });
 
-            assert.equal(err, null);
-            assert.equal(result, 1000);
-            done();
-        });
+  it("load scripts from an object", function (done) {
+    const scripts = { "script-one": "return 1000;" };
+    const s = new Scripto(redisClient);
+    s.load(scripts);
 
-    }));
+    s.run("script-one", [], [], function (err, result) {
+      assert.equal(err, null);
+      assert.equal(result, 1000);
+      done();
+    });
+  });
 
-    test('load a script from file', _clean(function(done) {
+  it("load a script from file", function (done) {
+    const s = new Scripto(redisClient);
+    s.loadFromFile("read-write", path.resolve(scriptDir, "read-write.lua"));
 
-        var s = new Scripto(redisClient);
-        s.loadFromFile('read-write', path.resolve(scriptDir, 'read-write.lua'));
+    s.run("read-write", ["helloKey"], [200], function (err, result) {
+      assert.equal(err, null);
+      assert.equal(result, 200);
+      done();
+    });
+  });
 
-        s.run('read-write', ['helloKey'], [200], function(err, result) {
+  it("load a script from dir", function (done) {
+    const s = new Scripto(redisClient);
+    s.loadFromDir(path.resolve(scriptDir));
 
-            assert.equal(err, null);
-            assert.equal(result, 200);
-            done();
-        });
-
-    }));
-
+    s.run("read-write", ["helloKey"], [200], function (err, result) {
+      assert.equal(err, null);
+      assert.equal(result, 200);
+      s.run("read-write-copy", ["helloKey"], [200], function (err, result) {
+        assert.equal(err, null);
+        assert.equal(result, 200);
+        done();
+      });
+    });
+  });
 });
-
-function _clean(callback) {
-
-    return function(done) {
-
-        redisClient.flushdb(function(err) {
-
-            if(err) throw err;
-            callback(done);
-        });
-    };
-}
